528d2eb0bdab0842617939c784314a8cfb7e6da7
Israel Torres <israel.a.torres01@gmail.com>
2019-03-12 21:51:45 -0400

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { DataTableComponent } from './components/data-table/data-table.component';
+import { ExporterService } from './services/exporter.service';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+  MatNativeDateModule,
+  MatTableModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatTableModule
   ],
-  providers: [],
+  providers: [ExporterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
